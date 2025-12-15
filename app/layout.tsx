08f8be78c9ee7b2a31b419c8aebdbc7cd3558a13
dabioly <mario-camarena12@hotmail.com>
2025-12-14 21:01:35 -0600

    icon + resume fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,10 @@ export const metadata: Metadata = {
   description:
     "Mario Camarena's portfolio - Master's student at UTRGV researching computer vision and exploring DevOps",
   icons: {
-    icon: '/icons/icon.png',
+    icon: [
+      { url: '/icons/icon.png', type: 'image/png' },
+      { url: '/icons/favicon.svg', type: 'image/svg+xml' },
+    ],
     shortcut: '/icons/icon.png',
     apple: '/icons/icon.png',
   },
@@ -21,6 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icons/icon.png" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700,900&display=swap"
           rel="stylesheet"
